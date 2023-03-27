1334722546b9604efe50fe00b46a945bea14edbb
XerXX9 <129093074+XerXX9@users.noreply.github.com>
2023-03-27 22:44:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const images = document.getElementsByClassName("image");
+
+let globalIndex = 0;
+    last = {x: 0, y: 0};
+
+const activate = (image,x,y) => {
+    image.style.left = `${x}px`;
+    image.style.top = `${y}px`;
+
+    image.dataset.status = "active";
+
+    last = {x,y};
+
+}
+
+const distanceFromLast = (x,y) => {
+    return Math.hypot(x - last.x, y - last.y);
+}
+
+window.onmousemove = e => {
+    if(distanceFromLast(e.clientX, e.clientY) > 100){
+        const lead = images[globalIndex % images.length],
+              tail = images[(globalIndex - 5) % images.length];
+        activate(lead, e.clientX, e.clientY);
+
+        if(tail) tail.dataset.status = "inactive";
+
+        globalIndex++;
+    }
+}
+
+window.addEventListener('scroll', () => {
+    const landing = document.querySelector('.landing');
+    const scrollScreen = document.querySelector('.scroll-screen');
+    const height = window.innerHeight;
+    const scrollPos = window.scrollY;
+    const scrollScreenPos = scrollScreen.offsetTop;
+
+    if (scrollPos >= scrollScreenPos - height / 2) {
+        landing.style.display = 'none';
+    } else {
+        landing.style.display = 'flex';
+    }
+});
